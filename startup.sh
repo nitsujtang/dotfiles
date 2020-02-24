a92a9089adc7e6ab7e914d609216862629ca3299
nitsujtang <justinqountang@gmail.com>
2020-02-24 12:35:58 -0800

    remove xwrits from startup, xinput keeps changing

--- startup.sh
+++ startup.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # set profile to battery life
-system76-power profile battery
+# system76-power profile battery
 
 # increase mouse accel in i3 to make mouse feel faster
-xinput --set-prop 18 281 0.55
+xinput --set-prop 18 282 0.55
 
 # set main output display to 60Hz and dpi 220
 xrandr --output eDP1 --refresh 60.00 --dpi 220
@@ -12,6 +12,4 @@ xrandr --output eDP1 --refresh 60.00 --dpi 220
 pactl set-sink-mute 0 1 # mute sound
 pactl set-source-mute 1 1 # mute mic
 
-sleep 1 && xbacklight -set 12
-
-xwrits typetime=30 breaktime=:30 canceltime=1 +multiply +mouse &
+sleep 1 && xbacklight -set 22
